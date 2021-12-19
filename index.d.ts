45742b29a1b67d7d9ca5d4bca499f9438f204cb0
A5rocks <git@helvetica.moe>
2021-12-19 12:48:05 +0900

    Update types for URL transformers

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-export const render: (md: string) => string;
+export const render: (md: string, urlTransform?: (string, string) => string) => string;
 export const toPlainText: (str: string) => string;
