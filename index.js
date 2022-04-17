b27f4952559fe89e51ab05ae2abf37b930b0fd37
A5rocks <git@helvetica.moe>
2022-04-17 16:30:10 +0900

    Process style for url changes too

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const hljs = require("highlight.js");
 const { marked } = require("marked");
+const { parseFragment, serialize } = require("parse5");
 const xss = require("xss");
+const { compile, serialize: cssSerialize, stringify, middleware } = require('stylis');
 
 const onTagAttr = (tag, name, value) => {
   if (tag === "link" && name === "rel")
@@ -32,6 +34,18 @@ const whiteList = {
 // Enable custom classes & ids to assist with CSS
 for (const key of Object.keys(whiteList)) whiteList[key].push("class", "id");
 
+const parseNode = (node, urlTransform) => {
+  if (node.nodeName === "#text" && node.parentNode.nodeName === "style") {
+    node.value = cssSerialize(compile(node.value), middleware([(elem) => {
+      if (elem.type === "decl" && elem.children.startsWith("url(")) {
+        elem.return = `${elem.props}:url(${urlTransform(elem.children.slice(4, -1), "style")})`
+      }
+    }, stringify]))
+  }
+
+  if (node.childNodes) node.childNodes.forEach((node) => parseNode(node, urlTransform));
+}
+
 /** Safely render markdown. */
 exports.render = (md, urlTransform = undefined) => {
   if (!urlTransform)
@@ -50,10 +64,10 @@ exports.render = (md, urlTransform = undefined) => {
       return urlTransform(value, tag) ? `href="${urlTransform(value, tag)}"` : "";
     
     if (tag === "a" && name === "href")
-      return urlTransform(value, tag) ? `href="${urlTransform(value, tag)}"` : ""
+      return urlTransform(value, tag) ? `href="${urlTransform(value, tag)}"` : "";
   }
 
-  return xss(
+  const tree = parseFragment(xss(
     marked(md, {
       highlight: (code, lang) =>
         lang && hljs.getLanguage(lang)
@@ -66,7 +80,11 @@ exports.render = (md, urlTransform = undefined) => {
       onTagAttr: closureOnTagAttr,
       whiteList,
     }
-  );
+  ));
+
+  tree.childNodes.forEach((node) => parseNode(node, urlTransform));
+
+  return serialize(tree);
 }
 
 exports.toPlainText = (str) =>
